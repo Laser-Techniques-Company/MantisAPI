0744af9c7f6f2516bd8c97f0cf8de38b624b1331
kaedenbrinkman <49798040+kaedenbrinkman@users.noreply.github.com>
2021-09-09 13:03:43 -0700

    Added more issues functions

--- src/MantisAPI.php
+++ src/MantisAPI.php
@@ -45,6 +45,7 @@ class MantisAPI
         json_decode($string);
         return (json_last_error() == JSON_ERROR_NONE);
     }
+    # ----------- ISSUES --------------
     public function getIssues($page_size = 10, $page = 1)
     {
         return $this->callAPI('issues?page_size=' . $page_size . '&page=' . $page, 'GET');
@@ -60,5 +61,69 @@ class MantisAPI
     public function createIssue($data)
     {
         return $this->callAPI('issues', 'POST', $data);
+    }
+    public function getIssueFiles($issue_id)
+    {
+        return $this->callAPI('issues/' . $issue_id . '/files', 'GET');
+    }
+    public function getIssueFile($issue_id, $file_id)
+    {
+        return $this->callAPI('issues/' . $issue_id . '/files/' . $file_id, 'GET');
+    }
+    public function getProjectIssues($project_id)
+    {
+        return $this->callAPI('issues?project_id=' . $project_id, 'GET');
+    }
+    public function getFilteredIssues($filter_id)
+    {
+        return $this->callAPI('issues?filter_id=' . $filter_id, 'GET');
+    }
+    public function getMyAssignedIssues()
+    {
+        return $this->callAPI('issues?filter_id=assigned', 'GET');
+    }
+    public function getMyReportedIssues()
+    {
+        return $this->callAPI('issues?filter_id=reported', 'GET');
+    }
+    public function getMyMonitoredIssues()
+    {
+        return $this->callAPI('issues?filter_id=monitored', 'GET');
+    }
+    public function getUnassignedIssues()
+    {
+        return $this->callAPI('issues?filter_id=unassigned', 'GET');
+    }
+    public function updateIssue($id, $data)
+    {
+        return $this->callAPI('issues/' . $id, 'PATCH', $data);
+    }
+    public function addAttachmentsToIssue($issue_id, $data)
+    {
+        return $this->callAPI('issues/' . $issue_id . '/files', 'POST', $data);
+    }
+    public function createIssueNote($issue_id, $data)
+    {
+        return $this->callAPI('issues/' . $issue_id . '/notes', 'POST', $data);
+    }
+    public function deleteIssueNote($issue_id, $note_id)
+    {
+        return $this->callAPI('issues/' . $issue_id . '/notes/' . $note_id, 'DELETE');
+    }
+    public function monitorIssue($issue_id)
+    {
+        return $this->callAPI('issues/' . $issue_id . '/monitors', 'POST');
+    }
+    public function addTagsToIssue($issue_id, $data)
+    {
+        return $this->callAPI('issues/' . $issue_id . '/tags', 'POST', $data);
+    }
+    public function removeTagsFromIssue($issue_id, $data)
+    {
+        return $this->callAPI('issues/' . $issue_id . '/tags', 'POST', $data);
+    }
+    public function addIssueRelationship($issue_id, $data)
+    {
+        return $this->callAPI('issues/' . $issue_id . '/relationships/', 'POST');
     }
 }
